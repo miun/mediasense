aae57c9ff6d6b2349b9a34f9046bb24c245956f3
Timo <t.schroeder84@web.de>
2012-04-02 20:43:47 +0200

    momon-tag

--- eclipse_projects/MediaSensePlatformOpenSource/src/se/miun/mediasense/disseminationlayer/lookupservice/distributed/JoinMessage.java
+++ eclipse_projects/MediaSensePlatformOpenSource/src/se/miun/mediasense/disseminationlayer/lookupservice/distributed/JoinMessage.java
@@ -3,7 +3,7 @@ package se.miun.mediasense.disseminationlayer.lookupservice.distributed;
 import se.miun.mediasense.disseminationlayer.communication.Message;
 
 public class JoinMessage extends Message {
-	
+
 	public JoinMessage(String toIp, String fromIp) {
 		
 		this.type = Message.JOIN;
