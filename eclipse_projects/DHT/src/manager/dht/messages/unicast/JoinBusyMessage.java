2bf3d3bd6698f008933cf1683fa880cbfe7bbb0a
Timo <t.schroeder84@web.de>
2012-04-12 17:26:40 +0200

    fixing join_ack error

---
+++ eclipse_projects/DHT/src/manager/dht/messages/unicast/JoinBusyMessage.java
@@ -0,0 +1,15 @@
+package manager.dht.messages.unicast;
+
+import manager.Message;
+
+public class JoinBusyMessage extends Message {
+
+	public JoinBusyMessage(String from, String to) {
+		super(from, to, Message.JOIN_BUSY);
+	}
+	
+	public String toString() {
+		//Return message info
+		return super.toString("MSG-JOIN_BUSY"); 
+	}
+}
