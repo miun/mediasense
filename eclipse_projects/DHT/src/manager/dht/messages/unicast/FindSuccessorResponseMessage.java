314da68757a462f78b67f1328cd1eed3cd6a32e7
Timo <t.schroeder84@web.de>
2012-04-17 20:14:13 +0200

    fixed successor link and showFinger() and added node deletion with broadcast

---
+++ eclipse_projects/DHT/src/manager/dht/messages/unicast/FindSuccessorResponseMessage.java
@@ -0,0 +1,32 @@
+package manager.dht.messages.unicast;
+
+import manager.Message;
+import manager.dht.NodeID;
+
+public class FindSuccessorResponseMessage extends Message {
+	private String networkAddress;
+	private NodeID hash;
+	
+	public FindSuccessorResponseMessage(String from,String to,String networkAddress,NodeID hash) {
+		super(from,to,FIND_SUCCESSOR_RESPONSE);
+		this.networkAddress = networkAddress;
+		this.hash = hash;
+	}
+
+	public String getNetworkAddress() {
+		return networkAddress;
+	}
+
+	public NodeID getHash() {
+		return hash;
+	}
+	
+	public String toString() {
+		//Return message info
+		return super.toString("MSG-PREDECESSOR_SUCCESSOR")+ " | sucKey: " + hash.toString() + " addr: (" + networkAddress.toString() + ")"; 
+	}
+	
+	//Return packet size for statistic
+	public int getDataAmount() {
+		return super.getDataAmount() + 4 + NodeID.ADDRESS_SIZE;
+	}}
