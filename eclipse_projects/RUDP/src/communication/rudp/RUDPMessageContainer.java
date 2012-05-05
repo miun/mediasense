338dcad3e6d00fc86ad564bab85b3c93ebd9506d
Timo <t.schroeder84@web.de>
2012-05-05 15:45:21 +0200

    merging

--- eclipse_projects/RUDP/src/communication/rudp/RUDPMessageContainer.java
+++ eclipse_projects/RUDP/src/communication/rudp/RUDPMessageContainer.java
@@ -1,70 +1,109 @@
 package communication.rudp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import communication.Message;
 
 public class RUDPMessageContainer {
 	//Flags
-	private static final int FLAG_RESET = 1;
+	//private static final int FLAG_RESET = 1;
 	private static final int FLAG_ACK = 2;
 	private static final int FLAG_DATA = 4;
 	private static final int FLAG_RESEND = 8;
+	private static final int FLAG_FRAGMENT = 16;
 	
-	private byte flags = 0;
+	//private boolean flag_reset = false;
+	private boolean flag_ack = false;
+	private boolean flag_data = false;
+	private boolean flag_resend = false;
+	private boolean flag_fragment = false;
+
 	private int seq_own;
 	private int seq_foreign;
 	private boolean[] ack_array;
+	private int frag_first;
+	private int frag_last;
+	
 	private Message msg;
 
-	public void setReset(boolean active) {
-		//Reset just consists of a flag
-		if(active) {
-			setFlag(FLAG_RESET);
+	//public void setReset(boolean active) {
+//		flag_reset = active;
+//	}
+	
+	public void setFragment(Integer first,Integer last) {
+		if(first == null || last == null) {
+			flag_fragment = false;
 		}
 		else {
-			resetFlag(FLAG_RESET);
+			flag_fragment = true;
+			this.frag_first = first;
+			this.frag_last = last; 
 		}
 	}
 	
 	public void setACK(boolean active,int seq,boolean[] ack_array) {
+		flag_ack = active;
+		
 		if(active) {
-			setFlag(FLAG_ACK);
 			seq_foreign = seq;
 			this.ack_array = ack_array; 
-		}
-		else {
-			resetFlag(FLAG_ACK);
 		}
 	}
 	
 	public void setData(boolean active,boolean resend,int seq,Message msg) {
+		flag_data = active;
+		flag_resend = resend;
+		
 		if(active) {
-			setFlag(FLAG_DATA);
-			setFlag(FLAG_RESEND);
 			this.seq_own = seq;
 			this.msg = msg;
 		}
-		else {
-			resetFlag(FLAG_DATA);
+	}
+	
+	public Boolean getFlag(int flag) {
+		switch(flag) {
+	//	case FLAG_RESET: return flag_reset;
+		case FLAG_ACK: return flag_ack;
+		case FLAG_DATA: return flag_data;
+		case FLAG_RESEND: return flag_resend;
+		case FLAG_FRAGMENT: return flag_fragment;
+		default: return null;
 		}
 	}
 
-	private void setFlag(int flag) {
-		flags |= flag;
-	}
-	
-	private void resetFlag(int flag) {
-		flags &= (-1 - flag);
-	}
-	
-	public boolean getFlag(int flag) {
-		//Extract flag
-		if((flags & flag) != 0) return true;
-		else return false;
-	}
-	
-	
-	
 	private byte[] serializePacket() {
+		ByteArrayOutputStream bos;
+		DataOutputStream dos;
+		
+		try {
+			bos = new ByteArrayOutputStream();
+			dos = new DataOutputStream(bos);
+			
+			//Write flags
+			dos.writeByte((flag_ack ? FLAG_ACK : 0) + (flag_data ? FLAG_DATA : 0) + (flag_resend ? FLAG_RESEND : 0) + (flag_fragment ? FLAG_FRAGMENT : 0));
+			
+			//Write ack field
+			if(flag_ack) {
+				
+			}
+			
+			//Write fragment fields
+			if(flag_fragment) {
+				
+			}
+			
+			//Write data
+			if(flag_data) {
+				
+			}
+			
+		}
+		catch(IOException e) {
+			
+		}
+		
 		return null;
 	}
 	
