da0e1239e87b0ec0a188119afa8655fb3e474cb1
Timo <t.schroeder84@web.de>
2012-05-11 20:29:16 +0200

    hardcore test

--- eclipse_projects/RUDP/src/communication/Main.java
+++ eclipse_projects/RUDP/src/communication/Main.java
@@ -1,13 +1,16 @@
 package communication;
 
 import java.net.InetAddress;
-import java.util.Random;
 
 import communication.rudp.socket.RUDPDatagram;
 import communication.rudp.socket.RUDPSocket;
 
 public class Main extends Thread {
 	private RUDPSocket sock;
+	public InetAddress dst;
+
+	public byte[] data;
+	public byte who = 'T';
 	
 	public static void main(String[] args) {
 		new Main();
@@ -20,42 +23,26 @@ public class Main extends Thread {
 		CommunicationInterface comm1 = new RUDP(core1);
 		CommunicationInterface comm2 = new RUDP(core2);*/
 
-		String who = "flo";
-		
-//		byte[] data = new byte[1024 * 1024];
+		data = new byte[1024];
 
 		//Message msg;
 		try {
 			RUDPDatagram dgram;
-			InetAddress dst = InetAddress.getByName("10.13.1.122");
+			dst = InetAddress.getByName("10.13.1.122");
 			sock = new RUDPSocket(23456);
 
 			//Create data packet
-			byte[] data = new byte[128];
-			for(int i = 0; i < 128; i++) data[i] = (byte)i;
+			for(int i = 0; i < data.length; i++) data[i] = (byte)i;
+			data[0] = who;
 			dgram = new RUDPDatagram(dst, 23456, data);
-			
-			Thread.sleep(1000);
 			
 			while(true) {
 				sock.send(dgram);
-				Thread.sleep(new Random().nextInt(300));
-
 			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		//Send data
-/*		msg = new TestMessage(comm1.getLocalIp(),comm2.getLocalIp(),null);
-		
-		try {
-			comm1.sendMessage(msg);
-		}
-		catch (DestinationNotReachableException e) {
-			
-		}*/
 		
 		//Shutdown
 		//comm1.shutdown();
@@ -68,7 +55,17 @@ public class Main extends Thread {
 		while(true) {
 			try {
 				data = sock.receive();
-				System.out.println("Data received with length " + data.length);
+				
+				if(data[0] != who) {
+					//Return data if its not from us!
+					try {
+						sock.send(new RUDPDatagram(dst, 23456, data));
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				//System.out.println("Data received with length " + data.length);
 			}
 			catch(DestinationNotReachableException e1) {
 				System.out.println("Destination not reachable");
