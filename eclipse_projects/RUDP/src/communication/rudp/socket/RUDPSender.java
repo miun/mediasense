06fcf514a600ba3945113c2c716286c3ac7ecd31
Timo <t.schroeder84@web.de>
2012-05-30 12:24:09 +0200

    merge

--- eclipse_projects/RUDP/src/communication/rudp/socket/RUDPSender.java
+++ eclipse_projects/RUDP/src/communication/rudp/socket/RUDPSender.java
@@ -15,7 +15,7 @@ import communication.rudp.socket.rangeset.DeltaRangeList;
 
 public class RUDPSender implements RUDPDatagramPacketSenderInterface {
 	private static final int MAX_DATA_PACKET_RETRIES = 5;
-	private static final int PACKET_FIRST_RETRY_PERIOD = 250;
+	private static final int PACKET_FIRST_RETRY_PERIOD = 25000000;
 	private static final int PERSIST_PERIOD = 1000;
 
 	//Link; Socket interface to send UDP datagrams
