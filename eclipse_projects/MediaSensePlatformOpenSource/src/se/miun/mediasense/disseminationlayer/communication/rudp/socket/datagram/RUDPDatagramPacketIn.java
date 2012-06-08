356f6e34fb0ed0b7071e12ccddd1aec216a6db59
Timo <t.schroeder84@web.de>
2012-06-08 17:22:05 +0200

    removed debug id from packet

--- eclipse_projects/MediaSensePlatformOpenSource/src/se/miun/mediasense/disseminationlayer/communication/rudp/socket/datagram/RUDPDatagramPacketIn.java
+++ eclipse_projects/MediaSensePlatformOpenSource/src/se/miun/mediasense/disseminationlayer/communication/rudp/socket/datagram/RUDPDatagramPacketIn.java
@@ -23,9 +23,6 @@ public class RUDPDatagramPacketIn extends RUDPDatagramPacket {
 		
 		//Read flag
 		try {
-			//TODO remove debug
-			id = dis.readInt();
-			
 			//Read and analyze flags
 			flag = dis.readByte();
 			flag_first = (flag & FLAG_FIRST) != 0 ? true : false; 
